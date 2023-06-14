8ec15e8bc6be9d1e96d03be7435fcbcd0726a345
ChenShisen <30495683+ChenShisen@users.noreply.github.com>
2023-06-14 20:42:15 +0800

    Update relu.cpp

--- haha/relu.cpp
+++ haha/relu.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-#include <xmmintrin.h>
+#include <immintrin.h>
 #include <chrono>
 
 void relu(float* input, float* output, int size) {
@@ -19,6 +19,23 @@ void relu_sse(float* input, float* output, int size) {
     }
 }
 
+void relu_sse_bf16(unsigned short* input, unsigned short* output, int size) {
+    __m128i zero = _mm_setzero_si128();
+    __m128i mask_16 = _mm_set1_epi16(0x7fff);
+    __m128i mask_32 = _mm_set1_epi32(0x7fff7fff);
+    __m128i input_32, in_value;
+    __m128 out_value;
+    #pragma omp parallel for
+    for (int i = 0; i < size; i += 8) {
+        input_32 = _mm_cvtepu16_epi32(_mm_loadu_si128((__m128i*)(input + i)));
+        in_value = _mm_castps_si128(_mm_cvtph_ps(input_32));
+        __m128 mask = _mm_cmpgt_ps(_mm_castsi128_ps(in_value), _mm_castsi128_ps(zero));
+        __mmask8 mask8 = _mm_movemask_ps(mask);
+        out_value = _mm_mask_mov_ps(_mm_castsi128_ps(in_value), mask8, _mm_castsi128_ps(zero));
+        _mm_storeu_si128((__m128i*)(output + i), _mm_packus_epi32(_mm_castps_si128(out_value), zero));
+    }
+}
+
 int main() {
     int size = 10000000;
     float* input = new float[size];
@@ -31,15 +48,29 @@ int main() {
     relu(input, output, size);
     auto end_time = std::chrono::steady_clock::now();
     auto time_diff = end_time - start_time;
-    std::printf("�~N~_�~K CPU 计�~W�~@~W�~W��~Z%.3lf �~R\n", std::chrono::duration<double>(time_diff).count());
-
+    std::printf("CPU 计算耗时：%.3lf 秒\n", std::chrono::duration<double>(time_diff).count());
+    
     start_time = std::chrono::steady_clock::now();
     relu_sse(input, output, size);
     end_time = std::chrono::steady_clock::now();
     time_diff = end_time - start_time;
-    std::printf("SSE �~L~G令�~X�~L~V计�~W�~@~W�~W��~Z%.3lf �~R\n", std::chrono::duration<double>(time_diff).count());
+    std::printf("SSE 指令优化单精度浮点数计算耗时：%.3lf 秒\n", std::chrono::duration<double>(time_diff).count());
+
+    unsigned short* input_bf16 = new unsigned short[size];
+    unsigned short* output_bf16 = new unsigned short[size];
+    for (int i = 0; i < size; i++) {
+        input_bf16[i] = i - (size / 2);
+    }
+
+    start_time = std::chrono::steady_clock::now();
+    relu_sse_bf16(input_bf16, output_bf16, size);
+    end_time = std::chrono::steady_clock::now();
+    time_diff = end_time - start_time;
+    std::printf("SSE 指令优化 BF16 计算耗时：%.3lf 秒\n", std::chrono::duration<double>(time_diff).count());
 
     delete[] input;
     delete[] output;
+    delete[] input_bf16;
+    delete[] output_bf16;
     return 0;
 }
